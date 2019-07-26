d0438a4c1325be526718f17891aad54d157aaaab
boxedlunch-us <tya0411@gmail.com>
2019-07-26 12:50:52 -0500

    Add files via upload

---
+++ Tag-Machine.ps1
@@ -0,0 +1,35 @@
+﻿param(
+    # Virtual machine to be tagged 
+    [Parameter(Mandatory=$true)]
+    [string]
+    $vm,
+    # Tag name
+    [Parameter(Mandatory=$true)]
+    [string]
+    $tag,
+    # Tag Category
+    [Parameter(Mandatory=$true)]
+    [string]
+    $tagCategory,
+    # Tag Category
+    [Parameter(Mandatory=$true)]
+    [string]
+    $vCenter,
+    # vCenter Credential; object type:(Get-Credential/PSCredential)
+    [Parameter(Mandatory=$false)]
+    [pscredential]
+    $vCenterCred,
+    # Username to be passed for quiet execution
+    [Parameter(Mandatory=$false)]
+    [string]
+    $user,
+    # Password to be passed for quiet execution
+    [Parameter(Mandatory=$false)]
+    [string]
+    $password
+)
+if($vCenterCred){
+    Connect-VIServer -Server $vCenter -Credential $vCenterCred
+}elseif($user -and $password){
+    Connect-VIServer -Server $vCenter -User $user -Password $password
+}
