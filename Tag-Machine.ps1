364a52842264ae3578f2c44df07dca785f04e4b4
Ricky Nelson <tya0411@gmail.com>
2019-07-27 19:32:28 -0500

    First iteration of script

--- Tag-Machine.ps1
+++ Tag-Machine.ps1
@@ -1,4 +1,29 @@
-﻿param(
+﻿<#
+.SYNOPSIS
+    Assigns tag to specified VM
+.DESCRIPTION
+    Checking for existence of tag, assigns tag or creates and assigns new tag to virtual machine
+.EXAMPLE
+    ##Example of use##
+.PARAMETER vm
+    Virtual Machine name
+.PARAMETER tag
+    IExisting or new tag
+.PARAMETER tagCategory
+    Existing tag category
+.PARAMETER vCenter
+    Name of vCenter Appliance
+.PARAMETER vCenterCred
+    (Only used interactively) credentials for vCenter in PSCredential form
+.PARAMETER user
+    Username for logging into vCenter
+.PARAMETER password
+    Plaintext password for logging into vCenter
+.NOTES
+    Author: Ricky Nelson
+    Date:  20190725
+#>
+param(
     # Virtual machine to be tagged 
     [Parameter(Mandatory=$true)]
     [string]
@@ -32,4 +57,25 @@ if($vCenterCred){
     Connect-VIServer -Server $vCenter -Credential $vCenterCred
 }elseif($user -and $password){
     Connect-VIServer -Server $vCenter -User $user -Password $password
+}else{
+    Connect-VIServer -server $vCenter
 }
+
+if(!$global:DefaultVIServers){
+    Write-Output "Not connected to vCenter server; terminating script"
+    break;
+}
+
+# Get tag specified in paramters. If tag does not exist, it is created with a placeholder
+$tagAssign = Get-Tag -Name $tag -Category $tagCategory -ErrorAction SilentlyContinue
+$vmAssign = get-vm -Name $vm
+
+if(!$tagAssign){
+    write-output "Tag does not exist. Creating..."
+    $tagAssign = new-tag -Name $tag -Category $tagCategory -Description "Description Pending"
+}
+
+# Assign specified or newly created tag
+New-TagAssignment -Tag $tagAssign -Entity $vmAssign
+
+Disconnect-VIServer $vCenter -Confirm:$false
